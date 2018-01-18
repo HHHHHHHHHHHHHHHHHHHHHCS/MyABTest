caa1c5284df200d50f6d3e3d9969aac90c956b99
HCS <464962683@qq.com>
2018-01-18 18:54:52 +0800

    manifest  的BUG修复

--- Assets/My/Scripts/Editor/Test.cs
+++ Assets/My/Scripts/Editor/Test.cs
@@ -14,7 +14,7 @@ public class Test : MonoBehaviour
         {
             Directory.CreateDirectory(dir);
         }
-        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows64);
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
         Debug.Log("打包完毕！！！");
